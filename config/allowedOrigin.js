7aa53212c057dc3119502587d17736b2dc294141
Chimelu <ezeoguinechimelu@gmail.com>
2023-11-14 15:36:08 +0100

    change

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     "https://task-beta-ruddy.vercel.app",
     "http://127.0.0.1:5500", // localhost public ip
     "http://localhost:5000",
-    "http://localhost:5173"
+    "http://localhost:5173",
+   " https://github.com/Chimelu/back-git.git"
   ];
   
-  module.exports = allowedOrigins;
+  module.exports = allowedOrigins; 
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 
-
 const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
