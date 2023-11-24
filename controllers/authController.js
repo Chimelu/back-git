b63f46e5f5b9883dc66ce0a3eec4cab6239b507d
Chimelu <ezeoguinechimelu@gmail.com>
2023-11-24 19:30:11 +0100

    j

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv/config')
 
 const newUser = async (req, res) => {
     try {
