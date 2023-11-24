f056cc86495e18ef1b2bd8b3456fc6e753ed7df0
Chimelu <ezeoguinechimelu@gmail.com>
2023-11-24 16:13:39 +0100

    change

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -36,50 +36,59 @@ const createTask = async (req, res) => {
 
 
 const getTask = async (req, res) => {
-    try {
-      const { id: taskID } = req.params;
-      const task = await Task.findOne({ _id: taskID });
-  
-      if (!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}` });
-      }
-  
-      res.status(200).json({ task });
-    } catch (err) {
-      res.status(500).json({ msg: err });
-    }
-  };
-  
+  try {
+    const { id: taskID } = req.params;
+    
+    // Ensure that the task belongs to the signed-in user
+    const task = await Task.findOne({ _id: taskID, user: req.user.id });
 
-// edit a task
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+
 const updateTask = async (req, res) => {
-    try {
-      const { id: taskID } = req.params;
-      const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, { new: true });
-  
-      if (!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}` });
-      }
-  
-      res.status(200).json({ task });
-    } catch (err) {
-      res.status(500).json({ msg: err });
-    } 
-  };
-  
+  try {
+    const { id: taskID } = req.params;
+    
+    // Ensure that the task belongs to the signed-in user
+    const task = await Task.findOneAndUpdate(
+      { _id: taskID, user: req.user.id },
+      req.body,
+      { new: true }
+    );
 
-const deleteTask=async(req,res)=>{
-    try{
-        const{id:taskID}=req.params
-        const task = await Task.findOneAndDelete({_id:taskID})
-        res.status(200).json({task})
-
-    } catch(err){
-        console.error('Error deleting task:', err);
-        res.status(500).json({msg: err})
-
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
     }
-   
-}
+
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+
+    // Ensure that the task belongs to the signed-in user
+    const task = await Task.findOneAndDelete({ _id: taskID, user: req.user.id });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (err) {
+    console.error('Error deleting task:', err);
+    res.status(500).json({ msg: err });
+  }
+};
 
 module.exports = {getAllTasks,createTask,getTask,updateTask,deleteTask}
