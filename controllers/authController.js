d7a2639be0cdb2b6ffac76cf94445fab65261a25
Chimelu <ezeoguinechimelu@gmail.com>
2023-11-26 19:59:25 +0100

    response change

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,7 +89,8 @@ const login = async (req, res) => {
 
      databody = json({
         user: user,
-        ttre: ttree
+        ttre: ttree,
+        code: "00"
     });
        
         res.status(200).json({
