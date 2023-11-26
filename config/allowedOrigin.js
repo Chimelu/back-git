5d0ea6ccf251e39c239b1413f9d329862ceb0579
Chimelu <ezeoguinechimelu@gmail.com>
2023-11-26 17:24:09 +0100

    a

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -3,7 +3,9 @@ const allowedOrigins = [
     "http://127.0.0.1:5500", // localhost public ip
     "http://localhost:5000",
     "http://localhost:5173",
-   " https://github.com/Chimelu/back-git.git"
+   "https://github.com/Chimelu/back-git.git",
+ "  https://taskbac.onrender.com"
+   
   ];
   
-  module.exports = allowedOrigins; 
+module.exports = allowedOrigins; 
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,19 +1,18 @@
    
-// Cross Origin Resource Sharing
 const allowedOrigins = require("./allowedOrigin");
 
 const corsOptions = {
   // origin parameter is whoever requested (ex: google.com)
   origin: (origin, callback) => {
-    // if the domain is in the whitelist
-    // '!origin' equiv to 'undefined', necessary b/c localhost doesn't serve an origin REMOVE AFTER DEVELOPMENT
+ 
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      // callback(error, origin_sent_back?)
+      
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
